e08a8458fc34ad838793d06efbf68e601bc9f84a
dpitt <dpitt@keyholesoftware.com>
2017-11-10 15:56:49 -0600

    temp login disablement

--- src/login/Login.js
+++ src/login/Login.js
@@ -12,7 +12,7 @@ export default class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      redirectToReferrer: false,
+      redirectToReferrer: true,
       loginError: false
     }
   }
