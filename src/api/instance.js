3937b0d7f642732083e2f155139ecb1ebf74ee09
dpitt <dpitt@keyholesoftware.com>
2017-11-10 15:07:58 -0600

    removed hard coded host name

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: 'http://'+location.hostname+':3001/api/',
   headers: { 'Content-Type': 'application/json' }
 })
 
